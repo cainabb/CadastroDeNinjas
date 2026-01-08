f681bf24fccd52e5287959592d9f395a1a0ed85f
cainabb <caina_bratz@hotmail.com>
2026-01-08 16:19:07 -0300

    Criando a Controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,20 @@
+package dev.java10x.CadastroDeNinjas;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem";
+    }
+
+
+
+
+
+}
