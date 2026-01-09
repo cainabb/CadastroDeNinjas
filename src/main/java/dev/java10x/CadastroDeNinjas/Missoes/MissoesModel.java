568afbbf214d5f4d4304d3245640faf984bc709d
cainabb <caina_bratz@hotmail.com>
2026-01-09 12:07:17 -0300

    fix: esse commit coloca a coluna email como unique

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,12 +3,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaController;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 
 @Entity
 @Table (name= "tb_missoes")
+@NoArgsConstructor //LOMBOK CRIAR UM NO ARGS CONSTRUCTOR
+@AllArgsConstructor //LOMBOK CRIA UM ALLARGS CONSTRUCTOR
+@Data //LOMBOK CRIA GETTERS E SETTERS
 
 
 public class MissoesModel {
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -3,10 +3,15 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity /*Transforma a CLASSE em uma ENTIDADE do Banco de Dados*/
 @Table(name="tb_cadastro")
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data //LOMBOK CRIA OS GETTERS E OS SETTERS
 public class NinjaModel {
 
     @Id
@@ -14,44 +19,11 @@ public class NinjaModel {
     private Long id;
     private String nome;
     private int idade;
+    @Column(unique = true)//CADA EMAIL TEM QUE SER ÚNICO, NÃO PODE TER 2 IGUAIS
     private String email;
 
     @ManyToOne /*UM NINJA TEM UMA ÚNICA MISSÃO, MANY NINJAS TEM ONE MISSION*/
     @JoinColumn(name="missoes_id") //Foreing Key ou Chave Estrangeira
     private MissoesModel missoes;
 
-
-    public NinjaModel(String nome) {
-        this.nome = nome;
-    }
-
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
